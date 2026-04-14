71d12bdc93513b5a4c07fe64301aa498c3ad7789
yeehawnted <80134123+yeehawnted@users.noreply.github.com>
2026-04-13 23:37:16 -0400

    Fix cursor issue
    Delete duplicate

--- static/lobby/lobby.js
+++ static/lobby/lobby.js
@@ -162,9 +162,4 @@ document.querySelectorAll('.ball').forEach(ball => {
 		this.style.transform = '';
 	});
 });
-import { retrievePlayerData } from "../player-api.js"
 
-const player = retrievePlayerData();
-
-//const playerNameCard = document.getElementById('player-name');
-document.getElementById('player-name').innerHTML = player.getName();
