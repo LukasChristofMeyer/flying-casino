3418e518f49b017a49b1994a60469045ffcad248
yeehawnted <80134123+yeehawnted@users.noreply.github.com>
2026-04-13 23:39:09 -0400

    Update lobby.js

--- static/lobby/lobby.js
+++ static/lobby/lobby.js
@@ -162,9 +162,3 @@ document.querySelectorAll('.ball').forEach(ball => {
 		this.style.transform = '';
 	});
 });
-import { retrievePlayerData } from "../player-api.js"
-
-const player = retrievePlayerData();
-
-//const playerNameCard = document.getElementById('player-name');
-document.getElementById('player-name').innerHTML = player.getName();
