2f90bcef6fd971a2e5556b1b783f4c5c4ad1112d
thorntongray <graytho@iu.edu>
2026-04-22 19:54:06 -0400

    Added constructors for the AI classes

--- static/cards/blackjackAI.js
+++ static/cards/blackjackAI.js
@@ -2,12 +2,16 @@ import {BlackjackHTMLHandler, Player} from "./blackjack.js"
 class BlackJackAI extends Player {
     AIIndex;
 
+    constructor (index) {
+        this.AIIndex = index;
+    }
+
     AIPlay () {
         BlackjackHTMLHandler.getValue(this.AIIndex);
         if (values[this.AIIndex] < 17) {
-            BlackjackHTMLHandler.hit(this.AIIndex)
+            BlackjackHTMLHandler.hit(this.AIIndex);
         } else {
-            BlackjackHTMLHandler.stay()
+            BlackjackHTMLHandler.stay();
         }
     }
 }
