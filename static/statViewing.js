a7459d28d2de933dbe0ec48082e71ee364545f2c
thorntongray <graytho@iu.edu>
2026-04-29 21:36:42 -0400

    potential fix again

--- static/statViewing.js
+++ static/statViewing.js
@@ -2,13 +2,15 @@ import { LocalPlayerData } from "./player-api.js";
 
 const statHover = document.getElementById("statHover");
 const popup = document.getElementbyId("popup");
+console.log(statHover);
 
 function showStats () {
     popup.innerHTML = "Name: " + LocalPlayerData.getName() + "<br>" + "Number of Wins: " + LocalPlayerData.getWins() + "<br>" + "Total Chips: " + LocalPlayerData.getChips();
     popup.style.visibility = 'visible';
+    console.log("hovering");
 }
 
-statHover.addEventListener("mouseover", showStats())
+statHover.addEventListener("mouseover", showStats());
 statHover.addEventListener("mouseout", () =>{
     popup.style.visibility = 'hidden';
 } )
